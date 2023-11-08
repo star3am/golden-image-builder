c02d4a34374faa7ee0bf2cee97fc66e3dd435278
Riaan Nolan <riaan.nolan@gmail.com>
2023-11-08 23:49:06 +0000

    building new container with terraform 1.6.3

--- terraform/awx-ansible-tower-instance/main.tf
+++ terraform/awx-ansible-tower-instance/main.tf
@@ -30,12 +30,14 @@ provider "google" {}
 
 module "aws_hashiqube" {
   # source                     = "star3am/hashiqube/hashicorp//modules/aws-hashiqube"
-  source                     = "git::https://github.com/star3am/terraform-hashicorp-hashiqube.git//modules/aws-hashiqube?ref=debug"
+  source                     = "star3am/hashiqube/hashicorp"
+  # source                     = "git::https://github.com/star3am/terraform-hashicorp-hashiqube.git//modules/aws-hashiqube?ref=debug"
   deploy_to_aws              = true
   aws_instance_type          = "t2.large"
   deploy_to_azure            = false
   deploy_to_gcp              = false
   debug_user_data            = true
+  # ssh_public_key             = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDIgkrDFTASaZYyJfGd0swmUZaol3JSZmO6D74230CtZjz/iNGKa+dY/6kvA0ABUhGZQu9a3LGD7tDMYmkKoAjpb177vwfgjTDMHwVKn2kGs/yqb7UMBMYtKP3FmmCUyGG5VUIaQIODuUSkgJ+sd0AptiMIQW+GrQ/KhcN5255mUb6rklWl83pYII1j+qgvDO/YrHq4QL34r5z4oPz9gO8f0l6kN8Hz6BEhAIzArg8kDI9lrsPXypCqlahE49cuzNH3C5GEC9MHo46U6+ZG2IOwCXjiP5ohnXyG8ho8N1BVpDa0xyVevy4COg4malaLVcRwP73YwgxiitNLWcq8k8nB riaan@Riaans-MacBook-Pro.local" 
   debug_allow_ssh_cidr_range = "0.0.0.0/0"
   whitelist_cidr             = "101.189.198.17/32"
   vagrant_provisioners       = "basetools,docker,minikube,ansible-tower"
